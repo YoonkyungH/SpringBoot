9293ce9d33bf8c4e4828d860528baea9017a339a
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2021-10-28 14:51:14 +0900

    주문 조회 V3.1 엔티티를 DTO로-페이징 한계 돌파

--- jpashop/src/main/java/jpabook/jpashop/repository/order/simplequery/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/order/simplequery/OrderApiController.java
@@ -1,4 +1,114 @@
 package jpabook.jpashop.repository.order.simplequery;
 
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
 public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {   // iter + tab으로 for문 자동생성
+            order.getMember().getName();        // LAZY 강제 초기화
+            order.getDelivery().getAddress();   // LAZY 강제 초기화
+
+            List<OrderItem> orderItems = order.getOrderItems();
+//            for (OrderItem orderItem : orderItems) {
+//                orderItem.getItem().getName();
+//            } // 아래의 람다식으로 바꾸기
+            orderItems.stream().forEach(o -> o.getItem().getName());    // LAZY 강제 초기화
+        }
+
+        return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        // orders를 OrderDto로 변환
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit)
+    {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Getter
+    private class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;                // address같은 경우에는 value object니까 그대로 노출시켜도 상관없지만
+        private List<OrderItemDto> orderItems;  // 주문 아이템같은 경우에는 껍데기만 DTO인게 아니라 그냥 DTO로 변한해 반환해야 함
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto {
+
+        private String itemName;    // 상품명
+        private int orderPrice;     // 주문 가격
+        private int count;          // 주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
 }
