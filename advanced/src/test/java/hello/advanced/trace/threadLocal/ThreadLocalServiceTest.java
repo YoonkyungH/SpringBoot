782817767b2afbdc479cf71fbaddca07cbcd97b0
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2021-12-19 17:16:03 +0900

    ThreadLocal

---
+++ advanced/src/test/java/hello/advanced/trace/threadLocal/ThreadLocalServiceTest.java
@@ -0,0 +1,44 @@
+package hello.advanced.trace.threadLocal;
+
+import hello.advanced.trace.threadLocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        // 스레드 실행 로직
+        Runnable userA = () -> {
+            service.logic("userA");
+        };
+        Runnable userB = () -> {
+            service.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000);    // 동시성 문제가 발생X
+        sleep(100);    // 동시성 문제4가 발생O
+        threadB.start();
+
+        sleep(2000);    // 메인 스레드 종료 대기
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- advanced/src/test/java/hello/advanced/trace/threadLocal/code/ThreadLocalService.java
+++ advanced/src/test/java/hello/advanced/trace/threadLocal/code/ThreadLocalService.java
@@ -3,16 +3,16 @@ package hello.advanced.trace.threadLocal.code;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class FieldService {
+public class ThreadLocalService {
 
-    private String nameStore;
+    private ThreadLocal<String> nameStore = new ThreadLocal<>();
 
     public String logic(String name) {
-        log.info("저장 name={} -> nameStore={}", name, nameStore);
-        nameStore = name;
+        log.info("저장 name={} -> nameStore={}", name, nameStore.get());
+        nameStore.set(name);
         sleep(1000);    // 저장하는데 1초 정도 걸린다고 가정한 것
-        log.info("조회 nameStore={}", nameStore);
-        return nameStore;
+        log.info("조회 nameStore={}", nameStore.get());
+        return nameStore.get();
     }
 
     private void sleep(int millis) {
