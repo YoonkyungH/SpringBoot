f6f70aaaf19ba0bd925d6f1fc60b6f8ec8701346
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2021-12-27 15:30:18 +0900

    전략 패턴(ContextV2Test)

---
+++ advanced/src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,53 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.ContextV2;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    /**
+     * 전략 패턴 적용
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());  // 전략을 파라미터로 전달 받는 방식
+        context.execute(new StrategyLogic2());
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스
+     */
+    @Test
+    void strategyV2() {
+        ContextV2 context = new ContextV2();
+        context.execute(new Strategy() {    // execute 안에서 실행할 코드 조각을 넘긴다고 생각
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스2, 람다
+     */
+    @Test
+    void strategyV3() {
+        ContextV2 context = new ContextV2();
+
+        context.execute(() -> log.info("비즈니스 로직1 실행"));
+        context.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+}
