8bb261838c7f66f968b04fd1c2ca4ecd0f701cb3
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2022-01-21 22:12:31 +0900

    임시 수정

--- noticeBoard/src/main/java/com/example/noticeBoard/domain/board/BoardResponseDto.java
+++ noticeBoard/src/main/java/com/example/noticeBoard/domain/board/BoardResponseDto.java
@@ -12,7 +12,8 @@ public class BoardResponseDto {
     private String content;
     private List<Long> fileId;
 
-    public BoardResponseDto(Board entity) {
+//     public BoardResponseDto(Board entity) {
+    public BoardResponseDto(Board entity, List<Long> fileId) {
         this.id = entity.getId();
         this.member = entity.getMember().getName();
         this.title = entity.getTitle();
