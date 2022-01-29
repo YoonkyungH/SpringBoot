88a7ada30549b92553c0e30e0b883dccbd61ac07
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2022-01-29 13:30:08 +0900

    어노테이션 기반으로 코드 변경, 세션 저장소 DB로 변경

--- prac-springboot/src/main/java/com/book/springboot/web/dto/IndexController.java
+++ prac-springboot/src/main/java/com/book/springboot/web/dto/IndexController.java
@@ -1,5 +1,7 @@
 package com.book.springboot.web.dto;
 
+import com.book.springboot.config.auth.LoginUser;
+import com.book.springboot.config.auth.dto.SessionUser;
 import com.book.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("username", user.getName());
+        }
 
         return "index";
     }
