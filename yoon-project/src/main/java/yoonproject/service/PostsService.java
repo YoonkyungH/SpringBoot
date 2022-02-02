87856c92f2d7710fd89a19ec0df3f2400153bdcc
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2022-02-02 21:42:50 +0900

    IndexController 추가

--- yoon-project/src/main/java/yoonproject/service/PostsService.java
+++ yoon-project/src/main/java/yoonproject/service/PostsService.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import yoonproject.domain.posts.Posts;
 import yoonproject.domain.posts.PostsRepository;
+import yoonproject.web.dto.PostsListResponseDto;
 import yoonproject.web.dto.PostsResponseDto;
 import yoonproject.web.dto.PostsSaveRequestDto;
 import yoonproject.web.dto.PostsUpdateRequestDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -38,5 +42,12 @@ public class PostsService {
                 );
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
---
+++ yoon-project/src/main/java/yoonproject/web/IndexController.java
@@ -0,0 +1,32 @@
+package yoonproject.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import yoonproject.config.auth.dto.SessionUser;
+import yoonproject.service.PostsService;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("username", user.getName());
+        }
+
+        return "index";
+    }
+
+}
