0e5dfbed3aacd0b0f97651c70eed0642687e9585
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2022-02-05 13:35:53 +0900

    google 로그인 후 userName 화면 노출 수정

--- yoon-project/src/main/java/yoonproject/web/IndexController.java
+++ yoon-project/src/main/java/yoonproject/web/IndexController.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import yoonproject.config.auth.dto.SessionUser;
 import yoonproject.service.PostsService;
+import yoonproject.web.dto.PostsResponseDto;
 
 import javax.servlet.http.HttpSession;
 
@@ -17,16 +19,28 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-//    public String index() {
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
 
         SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
-            model.addAttribute("username", user.getName());
+            model.addAttribute("userName", user.getName());
         }
 
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
