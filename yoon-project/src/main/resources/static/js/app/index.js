b825ff14ac7e684afbb31f61375e1207e2b3da3c
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2022-02-05 13:35:00 +0900

    mustache 화면 추가

--- yoon-project/src/main/resources/static/js/app/index.js
+++ yoon-project/src/main/resources/static/js/app/index.js
@@ -22,7 +22,7 @@ var main = {
 
         $.ajax({
             type: 'POST',
-            url: '/api/v1/posts',
+            url: '/api/posts',
             dataType: 'json',
             contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
@@ -43,7 +43,7 @@ var main = {
 
         $.ajax({
             type: 'PUT',
-            url: '/api/v1/posts/'+id,
+            url: '/api/posts/'+id,
             dataType: 'json',
             contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
@@ -59,7 +59,7 @@ var main = {
 
         $.ajax({
             type: 'DELETE',
-            url: '/api/v1/posts/'+id,
+            url: '/api/posts/'+id,
             dataType: 'json',
             contentType:'application/json; charset=utf-8'
         }).done(function() {
